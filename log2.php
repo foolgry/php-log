9b40d762e5ce254e6c1897b06e3d53f31c1bd126
Foolgry <981852406@qq.com>
2020-09-19 19:54:14 +0800

    log2更新

--- log2.php
+++ log2.php
@@ -3,11 +3,15 @@
 
 class log2
 {
-    private static $dir = "./log/";
     const LEVEL_INFO = 'info';
     const LEVEL_DEBUG = 'debug';
     const LEVEL_ERROR = 'error';
     const LEVEL_WARN = 'warn';
+
+    //日志文件目录
+    const DIR = __DIR__ . './log/';
+    //日志记录级别
+    const WRITE_LEVEL = 'info';
 
 
     public static function info($data)
@@ -32,9 +36,27 @@ class log2
 
     public static function write($data, $level)
     {
-        $prefix = date("Y-m-d H:i:s") . " [$level] ";
-        $content = $prefix . var_export($data, true) . PHP_EOL;
-        $file_name = self::$dir . date("Y-m-d") . ".log";
-        file_put_contents($file_name, $content, FILE_APPEND);
+        if (self::isWrite($level)) {
+            $prefix = date("Y-m-d H:i:s") . " [$level] ";
+            $content = $prefix . var_export($data, true) . PHP_EOL;
+            $file_name = self::DIR . date("Y-m-d") . ".log";
+            file_put_contents($file_name, $content, FILE_APPEND);
+        }
+    }
+
+    private static function isWrite($level)
+    {
+        return self::compareLevel($level) >= 0;
+    }
+
+    private static function compareLevel($level)
+    {
+        $debug = 1;
+        $info = 2;
+        $warn = 3;
+        $error = 4;
+
+        $writeLevel = self::WRITE_LEVEL;
+        return $$level - $$writeLevel;
     }
 }
